bb7998eeb4850e3013ee9920a89c77bdf80c248d
Natalia Henao <nataliahenaor24@gmail.com>
2025-06-19 17:38:26 -0500

    feat: main template

---
+++ My-fritter-app/src/app/components/templates/main-template/main-template.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-template',
+  templateUrl: './main-template.component.html',
+  styleUrls: ['./main-template.component.scss']
+})
+export class MainTemplateComponent {
+  isSidebarCollapsed = false;
+
+  toggleSidebar(): void {
+    this.isSidebarCollapsed = !this.isSidebarCollapsed;
+  }
+} 
